a4f2576702fff485ae584a1d302261d84b84227e
yuyakondo <yuyakondo333@gmail.com>
2025-08-09 00:51:12 +0900

    remove CAT_API_KEY

--- app/fetch-image.ts
+++ app/fetch-image.ts
@@ -8,9 +8,7 @@ type Image = {
 
 // APIから画像を取得する関数
 export async function fetchImage(): Promise<Image> {
-  const res = await fetch('https://api.thecatapi.com/v1/images/search', {
-    headers: { 'x-api-key': CAT_API_KEY },
-  });
+  const res = await fetch('https://api.thecatapi.com/v1/images/search');
   const images = await res.json();
 
   console.log('fetchImage: 画像情報を取得しました', images);
